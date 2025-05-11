abcbb73278f648930b315ba4cad274dc7fd25002
chandraguptsharma1 <chandraguptsharma1@gmail.com>
2025-05-11 14:43:24 +0530

    first commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('./api/auth',authRoutes);
+app.use('/api/auth',authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
